9a586f94ed8ab04285763d1a8546c487abab8929
tom hikari <tomprescott1565@gmail.com>
2025-05-22 12:01:55 +0100

    Add deployment script and inbox contract tests
    - Implemented a deployment script in deploy.js using HDWalletProvider and Web3.
    - Created tests for the Inbox contract in inbox.test.js, including deployment, default message verification, and message updating functionality.

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,3 +1,37 @@
 const assert = require("assert");
-const {Web3} = require("web3");
+const { Web3 } = require("web3");
 const ganache = require("ganache");
+
+const web3 = new Web3(ganache.provider());
+
+const { abi, evm } = require("../compiler");
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+  inbox = await new web3.eth.Contract(abi)
+    .deploy({
+      data: evm.bytecode.object,
+      arguments: ["Hi There"],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
+});
+
+describe("Inbox", () => {
+  it("should deploy the contract", async () => {
+    assert.ok(inbox.options.address);
+  });
+
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "Hi There");
+  });
+
+  it("can change the message", async () => {
+    await inbox.methods.setMessage("bye").send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "bye");
+  });
+});
